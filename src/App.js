3b836635ac12b9fe2365374f12af6fcb5ad84fc4
Cephas Tokoli <matumasie@gmail.com>
2021-10-07 22:12:40 +0000

    new commit

--- src/App.js
+++ src/App.js
@@ -2,42 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import ProductCard from './components/templates/cards';
 import data from './data';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <left>
-          <div>
-            <a className='brand' href="index.html">Amazona</a>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <left>
+            <div>
+              <a className='brand' href="/">Amazona</a>
+            </div>
+          </left>
+          <right>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
+          </right>
+        
+        </header>
+        <main >
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+          <div className='row center'>
+          {data.products.map(product => (
+            <ProductCard
+              id = {product._id}
+              image ={product.image}
+              name={product.name}
+              price={product.price}
+              rating = {product.rating}
+              numReviews = {product.reviews}
+            />
+          ))}
           </div>
-        </left>
-        <right>
-        <a href="cart.html">Cart</a>
-        <a href="signin.html">Sign In</a>
-        </right>
-       
-      </header>
-      <main className='row center'>
-      {data.products.map(product => (
-        <ProductCard
-          id = {product._id}
-          image ={product.image}
-          name={product.name}
-          price={product.price}
-          rating = {product.rating}
-          numReviews = {product.reviews}
-        />
-      ))}
-      
-       
-      </main>
-      <footer>
-        <div className='row center'>
-          All Rights Reserved
-        </div>
-      </footer>
-    </div>
+        
+        
+        </main>
+        <footer>
+          <div className='row center'>
+            All Rights Reserved
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
