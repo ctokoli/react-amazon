556778f953d4d4ba9e212c883c94e286f8696aad
Cephas Tokoli <matumasie@gmail.com>
2021-10-07 22:25:32 +0000

    creating of home and product screen

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function HomeScreen() {
+    return ( 
+        <div>
+            <h2>
+                Home Screen
+            </h2>
+        </div>
+     );
+}
+
+export default HomeScreen;
