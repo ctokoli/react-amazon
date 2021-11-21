6211b5a5734a5adbd3d01b34e5ea111666f78deb
Cephas Tokoli <matumasie@gmail.com>
2021-11-21 21:06:01 +0000

    creating server side

---
+++ backend/data.js
@@ -0,0 +1,66 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Nike slim Shirt',
+            category: "Shirts",
+            image: "./assets/images/p1.jpg",
+            price: 120,
+            brand: "Nike",
+            rating: 4.5,
+            reviews: 10,
+            discription: "Nice high quality Nike shirt",
+            stock: 10
+        },
+        {
+            _id: '2',
+            name: 'Adidas fit Shirt',
+            category: "Shirts",
+            image: "./assets/images/p2.jpg",
+            price:  100,
+            brand: "Adidas",
+            rating: 4.0,
+            reviews: 9,
+            discription: "Nice quality slim fit Shirt",
+            stock: 0
+        },
+        {
+            _id: '3',
+            name: 'Lacoste free Shirt',
+            category: "Shirts",
+            image: "./assets/images/p3.jpg",
+            price:  90,
+            brand: "Lacoste",
+            rating: 4.5,
+            reviews: 10,
+            discription: "Nice high quality Lacoste shirt",
+            stock: 9
+        },
+        {
+            _id: '4',
+            name: ' Puma Pants',
+            category: "Pants",
+            image: "./assets/images/p4.jpg",
+            price: 150,
+            brand: "Puma",
+            rating: 3.5,
+            reviews: 8,
+            discription: "Nice high quality puma shirt",
+            stock: 15
+        },
+        {
+            _id: '5',
+            name: 'Timberlake slim Pants',
+            category: "Pants",
+            image: "./assets/images/p5.jpg",
+            price:  120,
+            brand: "Timberlake",
+            rating: 4.5,
+            reviews: 7,
+            discription: "Timberlake shirt",
+            stock: 8
+        }
+    ]
+}
+
+export default data;
