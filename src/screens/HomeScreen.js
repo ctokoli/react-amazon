f2afb6559bad33efc8b5f00643da6a95afa49f6f
Cephas Tokoli <matumasie@gmail.com>
2021-11-24 23:21:21 +0000

    added redux

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import ProductCard from "./components/templates/cards";
+import ProductCard from "components/templates/cards";
 import axios from 'axios';
 
 
